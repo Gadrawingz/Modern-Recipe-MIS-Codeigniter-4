588f034fd8f71d1cd5beb44f804631663ec7b325
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2022-05-23 23:51:32 +0200

    Update recipe_list.php

--- app/Views/recipe_list.php
+++ app/Views/recipe_list.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * @var string $page_title           The page title (automatically created by CI from the $data array)
- * @var string $page_subtitle        The page subtitle (automatically created by CI from the $data array)
- * @var array  $recipes              List of recipes (automatically created by CI from the $data array)
- * @var App\Entities\Recipe $recipe  One recipe (created by the foreach instruction)
+ * @var string $page_title                       The page title (automatically created by CI from the $data array)
+ * @var string $page_subtitle                    The page subtitle (automatically created by CI from the $data array)
+ * @var array $search                            Search criteria
+ * @var array $recipes                           List of recipes (automatically created by CI from the $data array)
+ * @var App\Entities\Recipe $recipe              One recipe (created by the foreach instruction)
+ * @var \CodeIgniter\Pager\PagerRenderer $pager  Pagination class instance
  */
 ?>
 <!DOCTYPE html>
@@ -37,31 +39,43 @@ article
 <main role="main" class="container">
     <div class="title">
         <h1>
-          <?= esc($page_title) ?>
-          <small class="text-muted"><?= esc($page_subtitle) ?></small>
-      </h1>
+            <?= esc($page_title) ?>
+            <small class="text-muted"><?= esc($page_subtitle) ?></small>
+        </h1>
     </div>
 
     <div class="container">
 
-    <h3>List of recipe by id</h3>
-    <ul>
-<?php foreach ($recipes as $recipe): ?>
-          <li><?= anchor('recipe/' . $recipe->id, $recipe->title) ?></li>
-<?php endforeach; ?>
-    </ul>
+        <h3>List of recipe</h3>
+        <div class="my-3">
+            <?= form_open('/', ['class' => 'form-inline']) ?>
+                <?= form_input('search_text',
+                               $search['text'] ?? '',
+                               ['class' => 'form-control my-1 mr-3', 'placeholder' => "Text"]) ?>
 
-    <h3>List of recipe by slug</h3>
-    <ul>
+                <?= form_label("Number per page", 'search_nb_per_page', ['class' => 'my-1 mr-2']) ?>
+
+                <?= form_input('search_nb_per_page',
+                               $search['nb_per_page'] ?? '',
+                               ['id' => 'search_nb_per_page', 'class' => 'form-control my-1 mr-3', 'style' => 'width:70px']) ?>
+
+                <?= form_submit('search_submit',
+                                "Search",
+                                ['class' => 'btn btn-outline-primary my-1']) ?>
+            <?= form_close() ?>
+        </div>
+
+        <ul>
 <?php foreach ($recipes as $recipe): ?>
-          <li><?= anchor('recipe/' . $recipe->slug, $recipe->title) ?></li>
+            <li><?= anchor('recipe/' . $recipe->slug, $recipe->title) ?></li>
 <?php endforeach; ?>
-    </ul>
+        </ul>
+
+        <?= $pager->links('default', 'bootstrap') ?>
 
     </div>
 
 </main>
-
 
 <footer>
     <p class="text-center">&copy; 2020 My recipe website</p>
